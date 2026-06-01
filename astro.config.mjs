32781ac8ff176cf2ab4a53698ed92bc1f30abab0
Daniel <daniel.adamov@student.howest.be>
2026-06-02 01:51:55 +0200

    Pages fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danieladam13.github.io',
-  base: '/Portofolio_DanielAdamov',
+  base: '/Portfolio_DanielAdamov',
   build: {
     assets: 'assets'
   },
