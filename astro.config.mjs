ef60686025b022ec085ae31f39eb39745676a691
Daniel <daniel.adamov@student.howest.be>
2026-06-01 23:59:40 +0200

    Github Pages support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: '#',
-  // base: '/',
+  site: 'https://danieladam13.github.io',
+  base: '/Portofolio_DanielAdamov',
   vite: {
     plugins: [tailwindcss()]
   },
